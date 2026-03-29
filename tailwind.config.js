352f0fb24801b62d2ba165e45ee1c9bd3fa038ea
William Hia <24082010043@student.upnjatim.ac.id>
2026-03-29 17:25:08 +0700

    update: perbaikan struktur tailwind + pindah output ke dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
