df7b3d7ab3b252e1feeb7250d9a2ab81ce6d67bb
William Hia <24082010043@student.upnjatim.ac.id>
2026-03-30 17:56:15 +0700

    commit index.htm dan proses login

---
+++ backend/auth/process_login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include '../config/koneksi.php';
+
+$username = strtolower(trim($_POST['username']));
+$password = trim($_POST['password']);
+
+$query = mysqli_query($conn, 
+    "SELECT * FROM users WHERE LOWER(username)=LOWER('$username')"
+);
+
+$user = mysqli_fetch_assoc($query);
+
+if ($user && password_verify($password, $user['password'])) {
+
+    $_SESSION['user'] = $user['username'];
+    $_SESSION['role'] = $user['role'];
+
+    setcookie("user", $user['username'], time() + (86400 * 7), "/");
+
+    header("Location: ../../index.html"); // atau index.php kalau kamu ubah
+    exit;
+
+} else {
+    echo "Login gagal";
+}
